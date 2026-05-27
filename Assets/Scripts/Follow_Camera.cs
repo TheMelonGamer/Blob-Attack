b83adfc3f20185ea527e7f005c16da1e50e2ee95
TheMelonGamer <TheMelonGamer@github.com>
2026-05-27 21:41:15 +0200

    Player is now a Prefab, Follow_Camera created, Camera follows the Player

---
+++ Assets/Scripts/Follow_Camera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Follow_Camera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset = new Vector3 (0, 2, -10);
+    [SerializeField]
+    private float smoothTime = 0.25f;
+
+    Vector3 currentVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            target.position + offset,
+            ref currentVelocity,
+            smoothTime
+            );
+    }
+}
