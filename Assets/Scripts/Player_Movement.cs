c25ca7704bcfac71a0cd833d3293cfa4910ba1ba
TheMelonGamer <TheMelonGamer@github.com>
2026-05-27 21:16:49 +0200

    Player can move

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 10f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,25 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.wKey.isPressed)
+        {
+            this.transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            this.transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
     }
 }
